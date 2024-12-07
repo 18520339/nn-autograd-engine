a91dc8b6d9f8812c5645bf5cfdb307b6e9a33828
Quan Dang <18520339@gm.uit.edu.vn>
2024-12-07 23:39:42 +0700

    Successfully train and refactor train_cubic.cpp to use value semantics for Dense layers

--- cpp/train_cubic.cpp
+++ cpp/train_cubic.cpp
@@ -5,8 +5,6 @@
 int main() {
     vector<vector<TensorPtr>> X_train; // Features: [2x³, 3x², -3x]
     vector<TensorPtr> y_train;         // Target: 2x³ + 3x² - 3x
-    int epochs = 100, batch_size = X_train.size();
-    double learning_rate = 0.01;
 
     for (double x = -2.0; x <= 1.0; x += 0.1) {
         X_train.push_back({make_shared<Tensor>(2 * pow(x, 3)),
@@ -16,12 +14,14 @@ int main() {
     }
 
     Sequential<TensorPtr> model( // Initialize model
-        {make_shared<Dense>(3, 4, "relu", Initializers::he_uniform, "Dense0"),
-         make_shared<Dense>(4, 3, "relu", Initializers::he_uniform, "Dense1"),
-         make_shared<Dense>(3, 1, "linear", Initializers::he_uniform, "Dense2")},
+        {Dense(3, 4, "relu", Initializers::he_uniform, "Dense0"),
+         Dense(4, 3, "relu", Initializers::he_uniform, "Dense1"),
+         Dense(3, 1, "linear", Initializers::he_uniform, "Dense2")},
         Loss<TensorPtr>::mean_squared_error);
-
     model.summary();
+
+    double learning_rate = 0.05;
+    int epochs = 100, batch_size = X_train.size();
     model.train(X_train, y_train, epochs, learning_rate, batch_size);
     return 0;
 }
