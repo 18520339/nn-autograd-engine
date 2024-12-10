42ced78d8577924ad4dffe7469a5cd7d75258f61
Quan Dang <18520339@gm.uit.edu.vn>
2024-12-11 01:28:34 +0700

    Refactor tensor creation in training process and update model initialization for clarity

--- train_cubic.cpp
+++ train_cubic.cpp
@@ -1,28 +1,28 @@
-#include "nn_autograd/layers.hpp"
-#include "nn_autograd/losses.hpp"
 #include "nn_autograd/models.hpp"
-#include "nn_autograd/optim.hpp"
 
 int main() {
     vector<vector<TensorPtr>> X_train; // Features: [2x³, 3x², -3x]
     vector<TensorPtr> y_train;         // Target: 2x³ + 3x² - 3x
 
     for (double x = -2.0; x <= 1.0; x += 0.1) {
-        X_train.push_back({make_shared<Tensor>(2 * pow(x, 3)),
-                           make_shared<Tensor>(3 * pow(x, 2)),
-                           make_shared<Tensor>(-3 * x)});
-        y_train.push_back(make_shared<Tensor>(2 * pow(x, 3) + 3 * pow(x, 2) - 3 * x));
+        X_train.push_back({Tensor::create(2 * pow(x, 3)), // 2x³
+                           Tensor::create(3 * pow(x, 2)), // 3x²
+                           Tensor::create(-3 * x)});      // -3x
+        y_train.push_back(Tensor::create(2 * pow(x, 3) + 3 * pow(x, 2) - 3 * x));
     }
 
-    Sequential<TensorPtr> model( // Initialize model
+    using OutputType = TensorPtr; // vector<TensorPtr> for one-hot encoded labels
+    Sequential<OutputType> model( // Initialize model
         {Dense(3, 4, "relu", Initializers::he_uniform, "Dense0"),
          Dense(4, 3, "relu", Initializers::he_uniform, "Dense1"),
          Dense(3, 1, "linear", Initializers::he_uniform, "Dense2")},
-        Loss<TensorPtr>::mean_squared_error);
+        Loss<OutputType>::mean_squared_error);
     model.summary();
 
     int epochs = 100, batch_size = X_train.size();
     LearningRateScheduler *lr_scheduler = new WarmUpAndDecayScheduler(0.05, 5, 10, 0.9);
     model.train(X_train, y_train, epochs, lr_scheduler, batch_size);
+
+    delete lr_scheduler;
     return 0;
 }
