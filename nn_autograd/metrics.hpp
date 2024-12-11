cd867e34d8e6c52553efc9393bff5ae16fc02330
Quan Dang <18520339@gm.uit.edu.vn>
2024-12-12 02:33:49 +0700

    Fix compile errors when OutputType = vector<TensorPtr>in Metrics

--- nn_autograd/metrics.hpp
+++ nn_autograd/metrics.hpp
@@ -1,13 +1,13 @@
-// metrics.hpp
 #pragma once
 #include "tensor.hpp"
 #include <algorithm>
 #include <iterator>
 #include <numeric>
+#include <variant>
 
 template <typename OutputType>
 class Metrics {
-public:
+public: // TODO: Inject calculation into training loop
     static double accuracy(const vector<OutputType> &y_trues, const vector<OutputType> &y_preds) {
         if constexpr (is_same_v<OutputType, vector<TensorPtr>>) { // Check for one-hot encoded labels
             int correct = 0;
@@ -15,17 +15,17 @@ public:
                 auto true_max = max_element(y_trues[i].begin(), y_trues[i].end()); // Find index of max value in true labels
                 auto pred_max = max_element(                                       // Find index of max value in predictions
                     y_preds[i].begin(), y_preds[i].end(),
-                    [](const TensorPtr &a, const TensorPtr &b) { return a->data < b->data; }
-                );
+                    [](const TensorPtr &a, const TensorPtr &b) { return a->data < b->data; });
                 size_t true_idx = distance(y_trues[i].begin(), true_max);
                 size_t pred_idx = distance(y_preds[i].begin(), pred_max);
                 if (true_idx == pred_idx) correct++;
             }
             return static_cast<double>(correct) / y_trues.size();
         }
-        return inner_product(
-           y_trues.begin(), y_trues.end(), y_preds.begin(), 0.0, plus<>(),
-           [](const TensorPtr &y_true, const TensorPtr &y_pred) { return y_true->data == y_pred->data ? 1.0 : 0.0; }
-        ) / y_trues.size();
+        auto binary_op2 = [](const variant<TensorPtr, vector<TensorPtr>> &y_true,
+                             const variant<TensorPtr, vector<TensorPtr>> &y_pred) {
+            return get<0>(y_true) == get<0>(y_pred) ? 1.0 : 0.0;
+        };
+        return inner_product(y_trues.begin(), y_trues.end(), y_preds.begin(), 0.0, plus<>(), binary_op2) / y_trues.size();
     }
 };
