c144ed0386a0cc88aac7a41ebffada5e91dfd081
Quan Dang <18520339@gm.uit.edu.vn>
2024-12-12 00:46:16 +0700

    Add str_to_any function for dynamic type conversion and update Xy_from_csv for auto-detection of data types

--- nn_autograd/dataset.hpp
+++ nn_autograd/dataset.hpp
@@ -8,6 +8,20 @@
 #include <vector>
 using namespace std;
 
+any str_to_any(const string &str) { // Convert string to int, double, or string
+    try {
+        size_t pos; // Number of characters processed
+        int int_value = stoi(str, &pos);
+        if (pos == str.size()) return int_value;
+    } catch (invalid_argument) {}
+    try {
+        size_t pos;
+        double double_value = stod(str, &pos);
+        if (pos == str.size()) return double_value;
+    } catch (invalid_argument) {}
+    return str;
+}
+
 pair<vector<vector<any>>, vector<any>> Xy_from_csv(const string &file_path, bool skip_header = true) {
     vector<vector<any>> X_raw;
     vector<any> y_raw;
@@ -23,20 +37,17 @@ pair<vector<vector<any>>, vector<any>> Xy_from_csv(const string &file_path, bool
         stringstream line_stream(line);
         vector<any> row;
 
-        while (getline(line_stream, cell_value, ',')) {
-            try {
-                row.push_back(stod(cell_value));
-            } catch (const invalid_argument &) {
-                row.push_back(cell_value);
-            }
-        }
-        X_raw.insert(X_raw.end(), row.begin(), prev(row.end()));
+        // Auto-detect the data type of each cell and store it in the row
+        while (getline(line_stream, cell_value, ','))
+            row.push_back(str_to_any(cell_value));
+        X_raw.emplace_back(row.begin(), row.end() - 1);
+
         if (row.back().type() == typeid(string)) {
             string class_label = any_cast<string>(row.back());
             if (class_to_index.find(class_label) == class_to_index.end()) // Not found
                 class_to_index[class_label] = class_index++;
             y_raw.push_back(class_to_index[class_label]);
-        } else y_raw.push_back(any_cast<int>(row.back()));
+        } else y_raw.push_back(row.back());
     }
     return {X_raw, y_raw};
 }
