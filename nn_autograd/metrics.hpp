6b8363392a5efd3ca05869a28e3917138b6fd7a1
Quan Dang <18520339@gm.uit.edu.vn>
2024-12-16 18:51:14 +0700

    Use double for Metric calculation instead of TensorPtr

--- nn_autograd/metrics.hpp
+++ nn_autograd/metrics.hpp
@@ -5,26 +5,32 @@
 #include <numeric>
 #include <variant>
 
-template <typename OutputType>
+using YTruesVariant = variant<vector<double>, vector<vector<int>>>;
+using YPredsVariant = variant<vector<double>, vector<vector<double>>>;
+
 class Metrics {
-public: // TODO: Inject calculation into training loop
-    static double accuracy(const vector<OutputType> &y_trues, const vector<OutputType> &y_preds) {
-        if constexpr (is_same_v<OutputType, vector<TensorPtr>>) { // Check for one-hot encoded labels
-            int correct = 0;
-            auto compare_func = [](const TensorPtr &a, const TensorPtr &b) { return a->data < b->data; };
-            for (size_t i = 0; i < y_trues.size(); ++i) {
-                auto true_max = max_element(y_trues[i].begin(), y_trues[i].end(), compare_func); // Find index of max value in true labels
-                auto pred_max = max_element(y_preds[i].begin(), y_preds[i].end(), compare_func); // Find index of max value in predictions
-                size_t true_idx = distance(y_trues[i].begin(), true_max);
-                size_t pred_idx = distance(y_preds[i].begin(), pred_max);
+public:
+    static double accuracy(const YTruesVariant &y_trues, const YPredsVariant &y_preds) {
+        if (holds_alternative<vector<vector<int>>>(y_trues) && get<1>(y_trues)[0].size() > 0) { // Check for one-hot encoded labels
+            int correct = 0, n_samples = get<1>(y_trues).size();
+            for (size_t i = 0; i < n_samples; ++i) {
+                vector<int> y_true = get<1>(y_trues)[i];
+                vector<double> y_pred = get<1>(y_preds)[i];
+                auto true_max = max_element(y_true.begin(), y_true.end(), [](const int &a, const int &b) { return a < b; });
+                auto pred_max = max_element(y_pred.begin(), y_pred.end(), [](const double &a, const double &b) { return a < b; });
+
+                // Find index of max value in true labels and predictions
+                int true_idx = distance(y_true.begin(), true_max), pred_idx = distance(y_pred.begin(), pred_max);
                 if (true_idx == pred_idx) correct++;
             }
-            return static_cast<double>(correct) / y_trues.size();
+            return static_cast<double>(correct) / n_samples;
         }
-        auto binary_op2 = [](const variant<TensorPtr, vector<TensorPtr>> &y_true,
-                             const variant<TensorPtr, vector<TensorPtr>> &y_pred) {
-            return get<0>(y_true) == get<0>(y_pred) ? 1.0 : 0.0;
+        // When activation is sigmoid and loss is binary_crossentropy, convert probabilities to binary values
+        auto binary_op2 = [](const int &y_true, const double &y_pred) {
+            int y_pred_binary = y_pred > 0.5 ? 1 : 0;
+            return y_true == y_pred_binary ? 1 : 0;
         };
-        return inner_product(y_trues.begin(), y_trues.end(), y_preds.begin(), 0.0, plus<>(), binary_op2) / y_trues.size();
+        vector<double> trues = get<0>(y_trues), preds = get<0>(y_preds);
+        return inner_product(trues.begin(), trues.end(), preds.begin(), 0.0, plus<>(), binary_op2) / trues.size();
     }
 };
--- nn_autograd/models.hpp
+++ nn_autograd/models.hpp
@@ -2,6 +2,8 @@
 #include "converters.hpp"
 #include "layers.hpp"
 #include "logging.hpp"
+#include "losses.hpp"
+#include "metrics.hpp"
 #include "optim.hpp"
 #include <variant>
 // #include <thread>
@@ -12,19 +14,19 @@ private:
     vector<TensorPtr> parameters;
     vector<Dense> layers; // Currently only support Fully connected layers
     function<TensorPtr(const vector<OutputType> &, const vector<OutputType> &)> loss_func;
-    unordered_map<string, function<double(const vector<OutputType> &, const vector<OutputType> &)>> metric_funcs;
+    unordered_map<string, function<double(const YTruesVariant &, const YPredsVariant &)>> metric_funcs;
     unordered_map<string, vector<any>> history;
 
     variant<TensorPtr, vector<TensorPtr>> forward(vector<double> inputs) { // Forward propagation for input features
         vector<TensorPtr> input_tensors = doubles_to_1d_tensors(inputs);
         for (int i = 0; i < layers.size(); ++i)
             input_tensors = layers[i].forward(input_tensors);
-        if constexpr (is_same_v<OutputType, TensorPtr>) return input_tensors[0];
+        if (is_same_v<OutputType, TensorPtr>) return input_tensors[0];
         return input_tensors;
     }
 
     variant<vector<TensorPtr>, vector<vector<TensorPtr>>>
-    get_batch_tensors(const variant<vector<double>, vector<vector<int>>> &inputs, const int &batch_start, const int &last_idx) {
+    get_batch_tensors(const YTruesVariant &inputs, const int &batch_start, const int &last_idx) {
         if (holds_alternative<vector<double>>(inputs)) {
             vector<TensorPtr> batch_tensors(last_idx - batch_start);
             for (int i = batch_start; i < last_idx; ++i)
@@ -39,6 +41,17 @@ private:
         return batch_tensors;
     }
 
+    using PredDataType = conditional_t<is_same_v<OutputType, TensorPtr>, double, vector<double>>;
+    PredDataType convert_output_to_data(const OutputType &output) const {
+        if constexpr (is_same_v<OutputType, TensorPtr>) return output->data;
+        else {
+            vector<double> cates_data;
+            for (const TensorPtr &tensor : output)
+                cates_data.push_back(tensor->data);
+            return cates_data;
+        }
+    }
+
 public:
     vector<TensorPtr> &get_parameters() { return parameters; }
     vector<Dense> &get_layers() { return layers; }
@@ -46,7 +59,7 @@ public:
 
     Sequential(const vector<Dense> &_layers, // Constructor
                function<TensorPtr(const vector<OutputType> &, const vector<OutputType> &)> _loss_func,
-               unordered_map<string, function<double(const vector<OutputType> &, const vector<OutputType> &)>> _metric_funcs = {})
+               unordered_map<string, function<double(const YTruesVariant &, const YPredsVariant &)>> _metric_funcs = {})
         : layers(_layers), loss_func(_loss_func), metric_funcs(_metric_funcs) {
         // Collect all parameters across layers
         for (auto &layer : layers) {
@@ -82,7 +95,6 @@ public:
         for (auto &layer : layers) {
             size_t num_params = layer.get_parameters().size();
             total_params += num_params;
-
             cout << left << setw(layer_width) << (" " + layer.get_name())
                  << right << setw(shape_width) << (" " + to_string(layer.get_output_size()))
                  << right << setw(params_width) << num_params << endl;
@@ -94,7 +106,7 @@ public:
         cout << string(total_width, '=') << endl;
     }
 
-    void train(const vector<vector<double>> &X_train, const variant<vector<double>, vector<vector<int>>> &y_train,
+    void train(const vector<vector<double>> &X_train, const YTruesVariant &y_train,
                const int &epochs = 100, const variant<LearningRateScheduler *, double> &learning_rate = 0.01,
                const int &batch_size = 1, const double &clip_value = 0.0) {
         int data_size = X_train.size(), steps_per_epoch = ceil(static_cast<double>(data_size) / batch_size);
@@ -113,21 +125,26 @@ public:
             for (int batch_start = 0, batch_step = 0; batch_start < data_size; batch_start += batch_size, ++batch_step, ++epoch_step) {
                 logger.start_batch();
 
-                // Prevents index out of range when batch_size doesn't divide the data size
-                int last_idx = min(batch_start + batch_size, data_size);
+                // Forward propagation to get predictions
+                int last_idx = min(batch_start + batch_size, data_size); // Avoid index out of range when batch_size doesn't divide data_size
+                vector<PredDataType> y_preds_data;                       // Convert predictions to correct type for metric calculation
                 vector<OutputType> predictions;
-                for (int i = batch_start; i < last_idx; ++i) // Forward pass
-                    predictions.push_back(get<(is_same_v<OutputType, TensorPtr>) ? 0 : 1>(forward(X_train[i])));
 
-                // Backpropagation and update parameters with Gradient Descent
+                for (int i = batch_start; i < last_idx; ++i) {
+                    OutputType forward_result = get<(is_same_v<OutputType, TensorPtr>) ? 0 : 1>(forward(X_train[i]));
+                    y_preds_data.push_back(convert_output_to_data(forward_result));
+                    predictions.push_back(forward_result);
+                }
+
+                // Backpropagation to calculate gradients
                 vector<OutputType> y_batch = get<(is_same_v<OutputType, TensorPtr>) ? 0 : 1>(get_batch_tensors(y_train, batch_start, last_idx));
                 TensorPtr loss = loss_func(y_batch, predictions);
                 loss->backward();
 
+                // Update parameters with Gradient Descent and Learning Rate Scheduling
                 if (holds_alternative<LearningRateScheduler *>(learning_rate))
                     scheduled_lr = (*get<0>(learning_rate))(epoch_step);
-                else
-                    scheduled_lr = get<1>(learning_rate);
+                else scheduled_lr = get<1>(learning_rate);
 
                 for (TensorPtr &param : parameters) {
                     // Clip gradients to ensure gradients stay within a manageable range, especially for ReLU
@@ -143,9 +160,14 @@ public:
                 if (holds_alternative<LearningRateScheduler *>(learning_rate))
                     history["learning_rate"].push_back(scheduled_lr);
 
+                // Convert y_batch to the correct type for metric calculation
+                vector<conditional_t<is_same_v<OutputType, TensorPtr>, double, vector<int>>> y_trues_data;
+                for (int i = batch_start; i < last_idx; ++i)
+                    y_trues_data.push_back(get<(is_same_v<OutputType, TensorPtr>) ? 0 : 1>(y_train)[i]);
+
                 unordered_map<string, double> metrics;
                 for (const auto &[name, func] : metric_funcs) {
-                    double metric_value = func(y_batch, predictions);
+                    double metric_value = func(y_trues_data, y_preds_data);
                     history[name].push_back(metric_value);
                     metrics[name] = metric_value;
                 }
@@ -156,10 +178,12 @@ public:
         logger.end_training();
     }
 
-    vector<OutputType> predict(const vector<vector<double>> &X) {
-        vector<OutputType> predictions;
-        for (const vector<double> &inputs : X)
-            predictions.push_back(get<(is_same_v<OutputType, TensorPtr>) ? 0 : 1>(forward(inputs)));
-        return predictions;
+    vector<PredDataType> predict(const vector<vector<double>> &X) {
+        vector<PredDataType> y_preds_data;
+        for (const vector<double> &inputs : X) {
+            OutputType forward_result = get<(is_same_v<OutputType, TensorPtr>) ? 0 : 1>(forward(inputs));
+            y_preds_data.push_back(convert_output_to_data(forward_result));
+        }
+        return y_preds_data;
     }
 };
